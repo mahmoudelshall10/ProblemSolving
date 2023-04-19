a349cb3eaf07f0290691a83ccf1bb18bbc52fa22
mahmoudelshall10 <mahmoudelshall@outlook.com>
2023-04-19 11:16:48 +0200

    add

--- src/ColorfulStonesSE.java
+++ src/ColorfulStonesSE.java
@@ -1,5 +1,31 @@
 import java.util.Scanner;
 
 public class ColorfulStonesSE {
-    Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
+        String inst = input.nextLine();
+        int ans = 0;
+
+        // for(char ch : s.toCharArray())
+        // {
+        //     for(char chs : inst.toCharArray())
+        //     {
+        //         if(ch == chs){
+        //             ans +=1;
+        //         } 
+        //     }   
+        // }
+        // ans +=1;
+        
+        for (int i = 0; i < inst.length(); i++) {
+            if(inst.charAt(i) == s.charAt(ans)){
+                ans +=1;
+            } 
+        }
+        ans +=1;
+        System.out.println(ans);
+}
+
 }
